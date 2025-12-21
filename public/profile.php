c958f2ac316efbfecf27553d9f9b729d61754f04
yh243233 <137877988+yh243233@users.noreply.github.com>
2025-12-21 16:36:42 +0900

    [wip]プロフィール画面のヘッダー部分を完成

--- public/profile.php
+++ public/profile.php
@@ -1,30 +1,16 @@
 <?php
-$siteTitle = 'HOME';
+$siteTitle = 'Profile';
 require __DIR__ . '/../app/view/layout/head.php';
 ?>
 
 <!-- 全体背景色 -->
 
-<body class="background_color">
+<body class="background_color_profile">
+
   <!-- ヘッダー -->
-  <header class="header">
-    <div class="header_main_wrap">
-      <div class="header_right_wrap">
-        <icon class="header_logo">M</icon>
-        <span class="header_title">match</span>
-      </div>
-      <div class="header_left_wrap">
-        <button class="/?header_login">
-          <a href="/login.php">ログイン</a>
-        </button>
-        <button class=" header_registration" href="/signin">
-          <a href="/signup.php">
-            無料で始める
-          </a>
-        </button>
-      </div>
-    </div>
-  </header>
+  <?php
+  require __DIR__ . '/../app/view/layout/profileHeader.php';
+  ?>
 
 </body>
 
